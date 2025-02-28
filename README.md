14f65e5dde56a8f5a9d2a58aef55147a1c02abc6
alex buh <xannykitten@gmail.com>
2025-02-28 09:22:00 +0100

    Zadanie 1

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 This repo has all the exercises I did as part of my university's Java course.
 
 # PL
-To repo zawiera wszystkie zadania z programowania obiektowego, które przerobiłam.
+To repo zawiera wszystkie zadania z programowania obiektowego, które przerobiłxm.
---
+++ lab01/src/Point.java
@@ -0,0 +1,15 @@
+public class Point {
+    public double x, y;
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+//    public String toString() {
+//        return "X = " + x + "\nY = " + y;
+//    }
+}
